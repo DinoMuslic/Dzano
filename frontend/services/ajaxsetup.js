2c20fdec3ae23fa44df52c41038e3eca3519c8ac
Dzanan Habibija <dzanan.habibija@stu.ibu.edu.ba>
2025-09-19 09:14:22 +0200

    console change

--- frontend/services/ajaxsetup.js
+++ frontend/services/ajaxsetup.js
@@ -5,7 +5,6 @@ $.ajaxSetup({
     const user = Utils.get_from_localstorage("user");
     if (user && user.token) {
       xhr.setRequestHeader("Authorization", "Bearer " + user.token);
-      console.log("✅ Authorization header set:", user.token);
     } else {
       console.warn("⚠️ No token found in localStorage.");
     }
